dca784afd475674d1db92ddc4ac4c9d32b2315db
thanhyk14 <thanhaxt342@gmail.com>
2023-11-11 18:32:28 +0700

    add login test api

---
+++ app/src/main/java/uet/app/mysound/AppData.java
@@ -0,0 +1,23 @@
+package uet.app.mysound;
+
+public class AppData {
+    private static AppData instance;
+    private LoginResponse loginResponse;
+
+    private AppData() {}
+
+    public static AppData getInstance() {
+        if (instance == null) {
+            instance = new AppData();
+        }
+        return instance;
+    }
+
+    public LoginResponse getLoginResponse() {
+        return loginResponse;
+    }
+
+    public void setLoginResponse(LoginResponse response) {
+        loginResponse = response;
+    }
+}
---
+++ app/src/main/java/uet/app/mysound/UserService.java
@@ -0,0 +1,17 @@
+package uet.app.mysound;
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface UserService {
+
+    @POST("api/me/")
+    Call<LoginResponse> loginUser(@Body LoginRequest loginRequest);
+
+
+    @POST("users/")
+    Call<RegisterResponse> registerUsers(@Body RegisterRequest registerRequest);
+
+}
